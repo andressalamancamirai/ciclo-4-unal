00d4e728bba9fa52665efe971158d6d3f83eb365
andressalamancamirai <33354132+andressalamancamirai@users.noreply.github.com>
2021-11-29 20:02:10 -0500

    Clase ficheros internos

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.File
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nombreArchivo = "test.txt"
+        val file = File(filesDir,nombreArchivo)
+
+        val fos = openFileOutput(nombreArchivo, Context.MODE_PRIVATE)
+        fos.write("Primera linea".toByteArray())
+        fos.write("Primera linea".toByteArray())
+        fos.write("Primera linea".toByteArray())
+        fos.write("Primera linea".toByteArray())
+        fos.write("Primera linea".toByteArray())
+        fos.close()
+
+
+        val inputStream: InputStream = file.inputStream()
+        val contenido = inputStream.bufferedReader().use{
+            it.readText()
+        }
+
+        println(contenido)
+
+        file.delete()
     }
+
 }
