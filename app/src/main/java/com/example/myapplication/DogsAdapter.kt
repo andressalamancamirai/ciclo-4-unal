ba0b44e19e3bf6f4a1c29203fe2934ad12b5dff6
andressalamancamirai <33354132+andressalamancamirai@users.noreply.github.com>
2021-12-07 17:34:13 -0500

    Cambios en clase

---
+++ app/src/main/java/com/example/myapplication/DogsAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DogsAdapter(val images:List<String>) : RecyclerView.Adapter<DogsAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = images[position]
+        holder.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_dog, parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    class ViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+        fun bind(image:String){
+            //itemView.ivDog.fromUrl(image)
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,73 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.databinding.ActivityMainBinding
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.yesButton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var imagesPuppies : List<String>
+    private lateinit var dogsAdapter: DogsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.searchBread.setOnQueryTextListener(this)
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String): Boolean {
+        searchByName(query.toLowerCase())
+        return true
+    }
+
+    private fun getRetrofil(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query:String){
+        doAsync {
+            val call = getRetrofil().create(APIService::class.java).getDogsByBreed("$query/images").execute()
+            val puppies = call.body() as DogsResponse
+            uiThread {
+                if(puppies.status == "success"){
+                    initDogs(puppies)
+                }else{
+                    showErrorDialog()
+                }
+            }
+        }
+    }
+
+    private fun initDogs(puppies:DogsResponse){
+        if(puppies.status == "success"){
+            imagesPuppies = puppies.images
+        }
+        dogsAdapter = DogsAdapter(imagesPuppies)
+        binding.rvDogs.setHasFixedSize(true)
+        binding.rvDogs.layoutManager = LinearLayoutManager(this)
+        binding.rvDogs.adapter = dogsAdapter
+    }
+    private fun showErrorDialog(){
+        alert("Ha ocurrido un error, intetenelo de nuevo"){
+            yesButton {  }
+        }.show()
+
     }
 }
